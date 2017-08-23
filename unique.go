f2c51cea3b3bb875a83e427eab01e607dd25ccf2
caixw <xnotepad@gmail.com>
2017-08-24 01:14:41 +0800

    添加更多的配置参数

--- unique.go
+++ unique.go
@@ -13,26 +13,29 @@ import (
 	"github.com/issue9/autoinc"
 )
 
-const (
-	resetTimeMax = 60 // 分种
-	resetStepMax = 10
-)
-
-var random = rand.New(rand.NewSource(time.Now().Unix()))
-
 // Unique 基于时间戳的唯一字符串。
 // 不能在一秒之内重置计数器。
 type Unique struct {
+	step   int64 // 计数器的最大步长
+	timer  int64 // 计数器的最大重置时间
+	random *rand.Rand
+
 	prefix string           // 时间戳字符串
 	ai     *autoinc.AutoInc // 自增值
 }
 
-// New 声明一个新的 Unique
-func New() *Unique {
-	step := int64(random.Intn(resetStepMax))
+// New 声明一个新的 Unique。
+// seed 随机种子；
+// step 计数器的最大步长；
+// timer 计数器的最长重置时间，单位秒
+func New(seed, step, timer int64) *Unique {
+	random := rand.New(rand.NewSource(seed))
 
 	u := &Unique{
-		ai: autoinc.New(1, step, 1000),
+		step:   step,
+		timer:  timer,
+		random: random,
+		ai:     autoinc.New(1, random.Int63n(step), 1000),
 	}
 
 	u.reset()
@@ -43,9 +46,9 @@ func New() *Unique {
 // 重置时间戳和计数器
 func (u *Unique) reset() {
 	u.prefix = strconv.FormatInt(time.Now().Unix(), 10)
-	u.ai.Reset(1, int64(random.Intn(resetStepMax)))
+	u.ai.Reset(1, u.random.Int63n(u.step))
 
-	restTime := time.Duration(random.Intn(resetTimeMax)) * time.Minute
+	restTime := time.Duration(u.random.Int63n(u.timer)) * time.Minute
 	time.AfterFunc(restTime, u.reset)
 }
 
@@ -59,7 +62,7 @@ func (u *Unique) Bytes() []byte {
 	return []byte(u.String())
 }
 
-var defaultUnique = New()
+var defaultUnique = New(time.Now().Unix(), 10, 60)
 
 // String 返回一个唯一的 ID
 func String() string {
