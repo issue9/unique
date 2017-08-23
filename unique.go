29b4522e205cd667e7665947aec32b0ca3ab9274
caixw <xnotepad@gmail.com>
2017-08-24 01:04:36 +0800

    使代码更加随机性

--- unique.go
+++ unique.go
@@ -13,9 +13,15 @@ import (
 	"github.com/issue9/autoinc"
 )
 
-const resetMax = 24 * 60
+const (
+	resetTimeMax = 60 // 分种
+	resetStepMax = 10
+)
 
-// Unique 基于时间戳的唯一字符串
+var random = rand.New(rand.NewSource(time.Now().Unix()))
+
+// Unique 基于时间戳的唯一字符串。
+// 不能在一秒之内重置计数器。
 type Unique struct {
 	prefix string           // 时间戳字符串
 	ai     *autoinc.AutoInc // 自增值
@@ -23,8 +29,10 @@ type Unique struct {
 
 // New 声明一个新的 Unique
 func New() *Unique {
+	step := int64(random.Intn(resetStepMax))
+
 	u := &Unique{
-		ai: autoinc.New(1, 1, 1000),
+		ai: autoinc.New(1, step, 1000),
 	}
 
 	u.reset()
@@ -35,9 +43,9 @@ func New() *Unique {
 // 重置时间戳和计数器
 func (u *Unique) reset() {
 	u.prefix = strconv.FormatInt(time.Now().Unix(), 10)
-	u.ai.Reset(1, 1)
+	u.ai.Reset(1, int64(random.Intn(resetStepMax)))
 
-	restTime := time.Duration(rand.Intn(resetMax)) * time.Minute
+	restTime := time.Duration(random.Intn(resetTimeMax)) * time.Minute
 	time.AfterFunc(restTime, u.reset)
 }
 
