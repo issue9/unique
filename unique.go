1ed3c3f8806ace7963a706041c1ca954ef3b3648
caixw <xnotepad@gmail.com>
2018-02-08 22:15:48 +0800

    根据 autoinc 作出的改变

--- unique.go
+++ unique.go
@@ -13,25 +13,29 @@ import (
 	"github.com/issue9/autoinc"
 )
 
+const (
+	formatBase = 10
+)
+
 // Unique 基于时间戳的唯一字符串。
 //
 // NOTE: 不能在一秒之内重置计数器。
 type Unique struct {
-	step   int64 // 计数器的最大步长
 	random *rand.Rand
 
+	prefix   string
 	timer    *time.Timer
 	duration int64
 
-	prefix string           // 时间戳字符串
-	ai     *autoinc.AutoInc // 自增值
+	ai   *autoinc.AutoInc
+	step int64
 }
 
 // New 声明一个新的 Unique。
 //
 // seed 随机种子；
 // step 计数器的最大步长，可以负数，为 0 会 panic；
-// duration 计数器的最长重置时间，单位秒。系统会在 [1,timer] 范围内重置计数器。
+// duration 计数器的最长重置时间，单位秒。系统会在 [1,timer] 范围内重置计数器；
 func New(seed, step, duration int64) *Unique {
 	random := rand.New(rand.NewSource(seed))
 
@@ -39,7 +43,6 @@ func New(seed, step, duration int64) *Unique {
 		step:     step,
 		duration: duration,
 		random:   random,
-		ai:       autoinc.New(1, random.Int63n(step), 1000),
 	}
 
 	u.reset()
@@ -49,8 +52,12 @@ func New(seed, step, duration int64) *Unique {
 
 // 重置时间戳和计数器
 func (u *Unique) reset() {
-	u.prefix = strconv.FormatInt(time.Now().Unix(), 10)
-	u.ai.Reset(1, u.random.Int63n(u.step))
+	u.prefix = strconv.FormatInt(time.Now().Unix(), formatBase)
+
+	if u.ai != nil {
+		u.ai.Stop()
+	}
+	u.ai = autoinc.New(1, u.random.Int63n(u.step), 1000)
 
 	dur := u.random.Int63n(u.duration)
 	if dur == 0 {
@@ -65,13 +72,13 @@ func (u *Unique) reset() {
 func (u *Unique) String() string {
 	id, err := u.ai.ID()
 	if err == nil {
-		return u.prefix + strconv.FormatInt(id, 10)
+		return u.prefix + strconv.FormatInt(id, formatBase)
 	}
 
 	u.timer.Stop()
 	u.reset()
 
-	return u.prefix + strconv.FormatInt(u.ai.MustID(), 10)
+	return u.prefix + strconv.FormatInt(u.ai.MustID(), formatBase)
 }
 
 // Bytes 返回 String() 的 []byte 格式
